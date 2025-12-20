8bb070b95e460430ad577b8f49182623a799f085
RabbitTank <moondust1196551872@gmail.com>
2025-12-20 21:44:11 +0800

    feature:good

---
+++ re008/Main.java
@@ -0,0 +1,54 @@
+package re008;
+
+public class Main {
+    public static void main(String[] args) {
+        // region static 
+        Student.name = "李四";
+        // 此时被加载进 方法区（JDK 8 及以上为元空间） 但是对象没有被创建
+        // 静态变量随着类加载而被加载，比对象先出现
+        // 此后所有 Student 的 name 均为 "李四"
+        // 被 static 修饰的成员变量 -> 静态变量
+        //      被 该类 所有对象共享！
+        // 如果加 final 则需要在类加载时被初始化/静态代码块 （需要规范ALLCAP）
+        // public static final String NAME = "李四"; 
+
+        // 对于静态方法 基本都是测试类以及工具类中
+        // 直接类名调用即可
+        // 静态方法中，只能访问静态
+        // 非静态方法可以访问所有
+        // 静态方法没有 this 关键字
+        CalUtil.cal(1, 3);
+        // 静态成员（变量 / 方法）属于类本身，不依赖对象实例，只要类被加载就能使用；
+        // 实例成员（变量 / 方法）属于对象实例，必须创建对象后才能通过对象访问。
+        // endregion
+    }
+}
+
+
+class Student{
+    public Student(int age) {
+        this.age = age;
+    }
+    static String name;
+    private int age;
+    public static String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public static void setName(String name) {
+        Student.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+}
+
+class CalUtil{
+
+    public static void cal(int num1, int num2){
+        System.out.println("输入了" + num1 + " " + num2);
+    }
+}
---
+++ re009/Main.java
@@ -0,0 +1,107 @@
+package re009;
+
+public class Main {
+    public static void main(String[] args) {
+        // region 继承
+        // java 为 单继承设计 支持多层继承
+        // 增加代码复用性
+        // 子类 extends 父类
+        // 子类还可以在父类基础上增加其他功能
+        // LihuaCat lihua = new Animal(); // 父类是不完全的
+
+        // 继承 可以继承成员变量 不能继承构造器 以及非私有的成员方法
+        // 对于方法 每个类都会有一个虚方法表 记录所有上级父类的 非static 非final 非private 的方法
+        // 例子： HashiqiDog (8虚+destory) Dog(7虚+gurd) Animal(5虚+eat+drink)
+        // Object(1,2,3,4,5)
+
+        // 注意 就近原则
+        // Fu{String hobby = "fu"}
+        // Zi{String hobby = "zi"}
+        // sout(hobby) // 就近
+        // sout(this.hobby) // 本类
+        // sout(super.hobby) // 当前类的直接父类（最近的父类）
+
+        // 方法的重写 即 虚方法表中的虚方法覆盖
+
+        // 子类初始化前会先调用父类构造方法
+        // 父类中的属性都是共性的内容，而且都是可以被子类继承下来的，
+        // 而父类中的空参构造就是给父类中的属性一个默认初始化，
+        // 如果没有这一步，子类在使用父类中的属性的时候就没有了值了
+        // 子类构造方法第一行是默认 隐藏的 super();
+        // 如想用父类有参构造 super(num1,num2);
+        Animal a = new BuouCat();
+        a.drink();
+        ;
+        a.eat();
+        LihuaCat lihua = new LihuaCat();
+        lihua.catchMice();
+        lihua.drink();
+        lihua.eat();
+        TaidiDog taidi = new TaidiDog();
+        taidi.drink();
+        taidi.eat();
+        taidi.gurd();
+        taidi.close();
+        HashiqiDog hashiqi = new HashiqiDog();
+        taidi.eat();
+        hashiqi.eat();
+        
+
+    }
+}
+
+class Animal {
+    void eat() {
+        System.out.println("eat");
+    }
+
+    void drink() {
+        System.out.println("drink");
+    }
+}
+
+class Cat extends Animal {
+    void catchMice() {
+        System.out.println("抓老鼠");
+    }
+}
+
+class BuouCat extends Cat {
+
+}
+
+class LihuaCat extends Cat {
+
+}
+
+class Dog extends Animal {
+    @Override
+    void eat(){
+        System.out.println("就要吃狗粮");
+    }
+    void gurd() {
+        System.out.println("看家");
+    }
+}
+
+class HashiqiDog extends Dog {
+    @Override
+    void eat(){
+        super.eat(); // HashiqiDog的eat为父级的eat
+    }
+
+    void destory() {
+        System.out.println(this.getClass() + "拆家");
+    }
+}
+
+class TaidiDog extends Dog {
+    @Override
+    void eat() {
+        System.out.println("就要吃TaidiDog的食物");
+    }
+
+    void close() {
+        System.out.println(this.getClass().getName() + "蹭一蹭");
+    }
+}
