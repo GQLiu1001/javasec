62075f43db4b3f9a9ed68c60c68a681dff7bd586
RabbitTank <moondust1196551872@gmail.com>
2025-12-24 21:00:46 +0800

    enum

--- javase/re020/Enum.java
+++ javase/re020/Enum.java
@@ -7,9 +7,9 @@ public class Enum {
     //
     // 枚举主要做的是信息标志和信息分类。
     // 枚举的特殊规则是：
-    //      在类中定义的枚举默认被 static 修饰，即使你没有显式写 static。
+    // 在类中定义的枚举默认被 static 修饰，即使你没有显式写 static。
     // 枚举类默认被 final 修饰，不能被继承。
-    // 枚举类继承了 java.lang.Enum 类。
+    // 枚举类继承了 java.lang.Enum 类。注意 java.lang.Enum已经重写了Object类的toString()方法
     // 枚举类可以有构造器、方法和字段，但构造器必须是私有的（private）。
     // 枚举类是多例设计模式。
 
@@ -33,8 +33,9 @@ public class Enum {
         THURSDAY(4, "星期四"),
         FRIDAY(5, "星期五"),
         SATURDAY(6, "星期六");
+
         // 枚举的自定义属性（值的部分）
-        private final int code;    // 数字编码
+        private final int code; // 数字编码
         private final String desc; // 中文描述
 
         // 私有构造器：初始化自定义属性（必须private，可省略）
@@ -55,7 +56,8 @@ public class Enum {
 
     public static void main(String[] args) {
         WeekDay today = WeekDay.WEDNESDAY;
-
+        // 枚举类重写了 toString() 方法？
+        System.out.println(today);
         switch (today) {
             case MONDAY:
                 System.out.println("Today is " + today.getDesc() + "." + "and code is " + today.getCode());
@@ -79,5 +81,78 @@ public class Enum {
                 System.out.println("Today is " + today.getDesc() + "." + "and code is " + today.getCode());
                 break;
         }
+
+        Student stu = new Student("Alice", 20);
+        System.out.println(stu);
+    }
+
+    public static class Student {
+        private String name;
+        private int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public Student(String name, int age) {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        @Override
+        public String toString() {
+            return "Student{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
+
     }
 }
+
+// class Student {
+// private String name;
+// private int age;
+
+// public String getName() {
+// return name;
+// }
+
+// public int getAge() {
+// return age;
+// }
+
+// public Student(String name, int age) {
+// this.name = name;
+// this.age = age;
+// }
+
+// public void setName(String name) {
+// this.name = name;
+// }
+
+// public void setAge(int age) {
+// this.age = age;
+// }
+
+// @Override
+// public String toString() {
+// return "Student{" +
+// "name='" + name + '\'' +
+// ", age=" + age +
+// '}';
+// }
+
+// }
